60117f14c36315d45ae304de9fac4fe47939236e
Taichone <86025871+Taichone@users.noreply.github.com>
2024-09-18 13:50:15 +0900

    Initial Commit

---
+++ iOS-training-Taichone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-training-Taichone
+//
+//  Created by 三木 太智 on 2024/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-training-TaichoneUITests/iOS_training_TaichoneUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_training_TaichoneUITestsLaunchTests.swift
+//  iOS-training-TaichoneUITests
+//
+//  Created by 三木 太智 on 2024/09/18.
+//
+
+import XCTest
+
+final class iOS_training_TaichoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
