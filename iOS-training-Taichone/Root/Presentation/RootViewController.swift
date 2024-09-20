aef10cd79e8e3328e4cfb81a549f5cce97e3ff0a
Taichone <86025871+Taichone@users.noreply.github.com>
2024-09-20 12:32:49 +0900

    refactor: 現時点で不要な lifecycle メソッドの override を削除
    Session6 の趣旨であるライフサイクルの流れの確認のために print を貼っていただけなので削除。

--- iOS-training-Taichone/Root/Presentation/RootViewController.swift
+++ iOS-training-Taichone/Root/Presentation/RootViewController.swift
@@ -10,28 +10,11 @@ import UIKit
 final class RootViewController: UIViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
-        print("viewDidLoad")
         view.backgroundColor = .gray // NOTE: Session6 の遷移の視認性のために指定
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("viewWillAppear")
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        print("viewWillLayoutSubviews")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print("viewDidLayoutSubviews")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        print("viewDidAppear")
         
         let storyboard = UIStoryboard(name: "Weather", bundle: nil)
         guard let viewController = storyboard.instantiateViewController(
@@ -43,15 +26,5 @@ final class RootViewController: UIViewController {
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.modalPresentationStyle = .fullScreen
         self.present(navigationController, animated: true)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        print("viewWillDisappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-        print("viewDidDisappear")
     }
 }
