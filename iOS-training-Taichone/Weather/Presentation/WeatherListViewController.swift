5c6af4bcba849ec6accb4b72087d132c9080578b
Taichone <86025871+Taichone@users.noreply.github.com>
2024-09-26 12:37:56 +0900

    feat: WeatherList

--- iOS-training-Taichone/Weather/Presentation/WeatherListViewController.swift
+++ iOS-training-Taichone/Weather/Presentation/WeatherListViewController.swift
@@ -59,9 +59,10 @@ final class WeatherListViewController: UIViewController {
             object: nil
         )
         
+        setTableView()
+        
         Task {
             await fetchAreaWeatherForecastList()
-            
         }
     }
     
@@ -73,18 +74,23 @@ final class WeatherListViewController: UIViewController {
                     withIdentifier: WeatherListViewCell.reuseIdentifier,
                     for: indexPath
                 ) as! WeatherListViewCell
+                
                 cell.apply(itemModel: item)
                 cell.separatorInset = .zero
                 return cell
             }
         )
         
-        tableView.register(WeatherListViewCell.self, forCellReuseIdentifier: WeatherListViewCell.reuseIdentifier)
+        tableView.register(
+            UINib(nibName: "WeatherListViewCell", bundle: nil),
+            forCellReuseIdentifier: WeatherListViewCell.reuseIdentifier
+        )
         tableView.dataSource = dataSource
         tableView.delegate = self
     }
     
-    @IBAction private func onTapReloadButton(_ sender: Any) {
+    
+    @IBAction func onTapReloadButton(_ sender: Any) {
         Task {
             await fetchAreaWeatherForecastList()
         }
@@ -105,7 +111,23 @@ final class WeatherListViewController: UIViewController {
     }
 }
 
-extension WeatherListViewController: UITableViewDelegate {}
+extension WeatherListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 92
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+        
+        // TODO: 天気予報詳細画面へ遷移
+        print("tapped: \(item.areaWeatherForecast.area)")
+    }
+}
 
 extension WeatherListViewController {
     func fetchAreaWeatherForecastList() async {
@@ -115,7 +137,7 @@ extension WeatherListViewController {
         do {
             // TODO: API からリストでもらい、反映する
             let areaWeatherForecastList = try await weatherForecastProvider.fetchWeatherAreaWeatherForecastList()
-            print(areaWeatherForecastList)
+            bind(areaWeatherForecasts: areaWeatherForecastList)
         } catch {
             let alertMessage = self.weatherErrorAlertMessage(from: error)
             self.showWeatherErrorAlert(alertMessage: alertMessage)
@@ -123,6 +145,20 @@ extension WeatherListViewController {
         
         self.loadingIndicator.stopAnimating()
         self.reloadButton.isEnabled = true
+    }
+    
+    private func bind(areaWeatherForecasts: [AreaWeatherForecast]) {
+        var snapShot = SnapShot()
+        
+        // NOTE: 現時点では複数 section は考慮していない
+        let section = WeatherListSectionModel(title: "地域別")
+        let items: [WeatherListItemModel] = areaWeatherForecasts.compactMap {
+            .init(areaWeatherForecast: $0)
+        }
+        
+        snapShot.appendSections([section])
+        snapShot.appendItems(items, toSection: section)
+        dataSource.apply(snapShot)
     }
     
     private func weatherErrorAlertMessage(from error: Error) -> String {
