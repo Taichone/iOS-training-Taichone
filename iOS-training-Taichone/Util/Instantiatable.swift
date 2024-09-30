37e45959a45a08c91211582c731f1b443c9d2f05
Taichone <86025871+Taichone@users.noreply.github.com>
2024-10-01 00:16:26 +0900

    feat: vc の storyboard からの初期化を助ける Instantiatable の定義

---
+++ iOS-training-Taichone/Util/Instantiatable.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+protocol Instantiatable {
+    associatedtype Args
+    init?(coder: NSCoder, args: Args)
+    static var storyboardName: String { get }
+    static func instantiateFromStoryboard(with args: Args) -> Self
+}
+
+extension Instantiatable where Self: UIViewController {
+    private static var _storyboardName: String {
+        if storyboardName.isEmpty {
+            return className
+        } else {
+            return storyboardName
+        }
+    }
+
+    private static var storyboard: UIStoryboard {
+        return UIStoryboard.init(name: _storyboardName, bundle: nil)
+    }
+
+    private static var className: String {
+        return String(describing: Self.self)
+    }
+
+    static func instantiateFromStoryboard(with args: Args) -> Self {
+        guard let vc = storyboard.instantiateInitialViewController(creator: { coder in
+            return Self.init(coder: coder, args: args)
+        }) else {
+            fatalError("Can not instantiate \(Self.className) from \(storyboardName).storyboard")
+        }
+        return vc
+    }
+}
